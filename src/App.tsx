4215a0c4931391d02000125579e5f514c0660620
Umut Dag <me@umtdg.com>
2026-04-06 14:10:50 +0300

    feat(ui): hide window on pressing esc key

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 import { LockScreen, QuickAccess, Setup } from "./pages";
 import { listen } from "@tauri-apps/api/event";
@@ -7,6 +8,15 @@ import { listen } from "@tauri-apps/api/event";
 export default function App() {
   const [isLocked, setIsLocked] = useState(true);
   const [isFirstLaunch, setIsFirstLaunch] = useState(false);
+
+  function handleKeyDown<T>(e: React.KeyboardEvent<T>) {
+    console.log("Got key: ", e.key);
+
+    if (e.key == "Escape") {
+      const currentWindow = getCurrentWindow();
+      currentWindow.hide();
+    }
+  }
 
   function checkState() {
     invoke<boolean>("is_first_launch").then(setIsFirstLaunch);
@@ -18,8 +28,12 @@ export default function App() {
   listen("state-changed", checkState);
 
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center bg-bg text-text">
-      {isFirstLaunch ? <Setup /> : (isLocked ? <LockScreen /> : <QuickAccess />)}
+    <div
+      tabIndex={-1}
+      onKeyDown={handleKeyDown}
+      className="h-screen w-full flex flex-col items-center justify-center bg-bg text-text"
+    >
+      {isFirstLaunch ? <Setup /> : isLocked ? <LockScreen /> : <QuickAccess />}
     </div>
   );
 }
