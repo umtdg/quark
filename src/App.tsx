4259aab02b6502479844a24631009d8d57387817
Umut Dag <me@umtdg.com>
2026-04-16 18:54:51 +0300

    feat: add name and version to bottom left on ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { getCurrentWindow } from "@tauri-apps/api/window";
+import { getVersion, getName } from "@tauri-apps/api/app";
 
 import { LockScreen, QuickAccess, Setup } from "./pages";
 import { listen } from "@tauri-apps/api/event";
@@ -8,6 +9,8 @@ import { listen } from "@tauri-apps/api/event";
 export default function App() {
   const [isLocked, setIsLocked] = useState(true);
   const [isFirstLaunch, setIsFirstLaunch] = useState(false);
+  const [version, setVersion] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(null);
 
   function handleKeyDown<T>(e: React.KeyboardEvent<T>) {
     if (e.key == "Escape") {
@@ -30,6 +33,11 @@ export default function App() {
     };
   });
 
+  useEffect(() => {
+    getVersion().then(setVersion);
+    getName().then(setName);
+  }, []);
+
   return (
     <div
       tabIndex={-1}
@@ -37,6 +45,9 @@ export default function App() {
       className="h-screen w-full flex flex-col items-center justify-center bg-bg text-text"
     >
       {isFirstLaunch ? <Setup /> : isLocked ? <LockScreen /> : <QuickAccess />}
+      <span className="flex-end self-start p-1 text-xs text-text/60">
+        {name ?? "Unknown"} v{version ?? "0.0.0"}
+      </span>
     </div>
   );
 }
